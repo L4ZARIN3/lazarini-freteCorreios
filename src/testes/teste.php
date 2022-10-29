47a6680c93310853f3424ab20cd9abc54bec609f
Bruno Lazarini Bezerra <35786017+L4ZARIN3@users.noreply.github.com>
2022-10-28 22:41:07 -0300

    Adicionada quantidade do mesmo produto

--- src/testes/teste.php
+++ src/testes/teste.php
@@ -6,15 +6,15 @@ use Lazarini\CorreiosFrete\CORREIOS;
 
 $instance = new CORREIOS();
 
-$x = $instance
-    ->service(CORREIOS::PAC)
+$frete = $instance
+    ->service(CORREIOS::SEDEX)
     ->origem('04474340')
     ->destino('01001000')
     ->formato(CORREIOS::CAIXA)
     ->maoPropria(false)
-    ->item('30', '30', '30', '0.5')
+    ->item('30', '30', '30', '1.00', 2)
     ->valorDeclarado(0)
     ->avisoRecebimento(false)
     ->calc();
 
-print_r($x);
+print_r($frete);
